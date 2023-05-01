b7c112ba97a10356e3db734b56e0b00f67f1ba0b
jstted <tedeev17@gmail.com>
2023-05-01 15:23:07 +0300

    Prepared for work

--- SweaterWeather/MainModule/MainViewController.swift
+++ SweaterWeather/MainModule/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SweaterWeather
 //
 //  Created by Mikhail Tedeev on 01.05.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
