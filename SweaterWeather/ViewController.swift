e78c6f8d143bc30d896f0e363ca0e341f83ce662
jstted <tedeev17@gmail.com>
2023-05-01 15:12:55 +0300

    Initial Commit

---
+++ SweaterWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweaterWeather
+//
+//  Created by Mikhail Tedeev on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
