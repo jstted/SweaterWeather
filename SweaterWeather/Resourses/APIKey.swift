33e4ead5fd4cfac9ccfc51e21b1cc8fdfde78904
jstted <tedeev17@gmail.com>
2023-05-02 19:43:13 +0300

    add api key to gitignore

---
+++ SweaterWeather/Resourses/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  SweaterWeather
+//
+//  Created by Mikhail Tedeev on 02.05.2023.
+//
+
+import Foundation
+
+enum APIKey: String {
+    case get = "5b688e6220b46375c4d795fc6bc8d86d"
+}
