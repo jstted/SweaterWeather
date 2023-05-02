612178a7738debc95c7ff2e16b174df91bf2031b
jstted <tedeev17@gmail.com>
2023-05-02 16:00:15 +0300

    add constraints to top elemets in main module

--- SweaterWeather/Application/AppDelegate.swift
+++ SweaterWeather/Application/AppDelegate.swift
@@ -12,7 +12,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {        return true
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
     }
 
     // MARK: UISceneSession Lifecycle
--- SweaterWeather/MainModule/MainViewController.swift
+++ SweaterWeather/MainModule/MainViewController.swift
@@ -11,8 +11,8 @@ final class MainViewController: UIViewController {
 
     //MARK: - UI
     private var backgroundImageView = UIImageView()
-    private var locationButton = UIButton()
-    private var searchButton = UIButton()
+    private var locationButton = UIButton(type: .system)
+    private var searchButton = UIButton(type: .system)
     private var searchTextField = UITextField()
     private var weatherGlyphImageView = UIImageView()
     private var temperatureLabel = UILabel()
@@ -21,6 +21,7 @@ final class MainViewController: UIViewController {
     //MARK: - Property
     var presenter: MainPresenter?
     
+    //MARK: - UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setViews()
@@ -38,13 +39,27 @@ extension MainViewController {
     private func setViews() {
         view.backgroundColor = .systemPink
         setupBackground()
+        setupTopButton(locationButton, image: "location.circle.fill")
+        setupTopButton(searchButton, image: "magnifyingglass.circle.fill")
+        setupSearchTextField()
     }
     
     private func setupBackground() {
         backgroundImageView.image = UIImage(named: "background")
         backgroundImageView.frame = view.bounds
         backgroundImageView.contentMode = .scaleAspectFill
-        view.addSubview(backgroundImageView)
+    }
+    
+    private func setupTopButton(_ button: UIButton, image: String) {
+        button.tintColor = .tintColor
+        button.setImage(UIImage(systemName: image), for: .normal)
+    }
+    
+    private func setupSearchTextField() {
+        searchTextField.placeholder = "Search city"
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.backgroundColor = UIColor(named: "backgroundColor")
+        searchTextField.textColor = .tintColor
     }
 }
 
@@ -62,6 +77,7 @@ extension MainViewController {
                                        weatherGlyphImageView,
                                        temperatureLabel,
                                        cityLabel,]
+        view.addSubview(backgroundImageView)
         subviewsArray.forEach { view in
             self.view.addSubview(view)
             view.translatesAutoresizingMaskIntoConstraints = false
@@ -71,6 +87,24 @@ extension MainViewController {
     private func setConstraints() {
         NSLayoutConstraint.activate([
             
+            //MARK: locationButton
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            locationButton.widthAnchor.constraint(equalToConstant: 34),
+            locationButton.heightAnchor.constraint(equalToConstant: 34),
+            
+            //MARK: searchButton
+            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            searchButton.widthAnchor.constraint(equalToConstant: 34),
+            searchButton.heightAnchor.constraint(equalToConstant: 34),
+            
+            //MARK: locationButton
+            searchTextField.centerYAnchor.constraint(equalTo: locationButton.centerYAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: locationButton.trailingAnchor,
+                                                     constant: 8),
+            searchTextField.trailingAnchor.constraint(equalTo: searchButton.leadingAnchor,
+                                                      constant: -8)
         ])
     }
 }
