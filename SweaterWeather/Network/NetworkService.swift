ed065ef6dd7d9097232b6e9ac24aa029c03ef91f
jstted <tedeev17@gmail.com>
2023-05-02 19:43:36 +0300

    add api key to gitignore

--- SweaterWeather/Network/NetworkService.swift
+++ SweaterWeather/Network/NetworkService.swift
@@ -13,7 +13,7 @@ protocol NetworkServiceProtocol {
 
 class NetworkService: NetworkServiceProtocol {
     func getCurrentWeather(parameters: [String: Any]?, complition: @escaping (Result<Weather?, Error>) -> Void) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=5b688e6220b46375c4d795fc6bc8d86d&units=metric"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=\(APIKey.get)&units=metric"
         guard let url = URL(string: urlString) else { return }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -26,10 +26,7 @@ class NetworkService: NetworkServiceProtocol {
                 return
             }
             do {
-                guard let data else {
-                    print("data nil")
-                    return
-                }
+                guard let data else { return }
                 let weather = try JSONDecoder().decode(Weather.self, from: data)
                 complition(.success(weather))
             } catch {
